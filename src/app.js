4b9ac2c870d6806348944b2bd1057238a7da5a64
aitor santaeugenia mari <asantaeugenia@uoc.edu>
2023-10-25 21:54:18 +0200

    fix: fixing issues

--- src/app.js
+++ src/app.js
@@ -4,49 +4,41 @@ const figCaption = document.getElementById("imageTrailerFigcaption");
 const overlayDiv = document.getElementById("overlayDiv");
 const imagePoster = document.getElementById("imagePoster");
 
+export function hoverImage(element, elementType){
+    if(elementType === 1){
+        element.setAttribute('src', '../src/assets/images/mindhunter_logo_white.png');
+    }else if(elementType === 2){
+        element.setAttribute('src', '../assets/images/mindhunter_logo_white.png');
+    }
+}
 
-addEventListener("load", (e) => {
+export function unhoverImage (element, elementType){
+    if(elementType === 1){
+        element.setAttribute('src', './src/assets/images/mindhunter_logo.svg');
+    }else if(elementType === 2){
+        element.setAttribute('src', './src/assets/images/mindhunter_logo.svg');
+    }
+}
+
+// FUNCTION TO HIDE THE MINDHUNTER POSTER AND SHOW THE TRAILER
+export function changeToVid(element) {
+    element.classList.add("hidden");
+    imagePoster.classList.add("hidden");
+    trailerVideo.classList.remove("hidden");
+    figCaption.innerHTML = `Tráiler oficial de Mindhunter. | Video proporcionado por <a href="https://www.youtube.com/watch?v=LR3G1lWbnUU" target="_blank">Netflix</a>`
+    trailerVideo.autoplay = "true";
+    trailerVideo.load();
+}
+
+//FUNCTION TO ADD AN OVERLAY WHEN HOVERING THE IMAGE
+export function addOverlay(element, e){
     e.preventDefault();
+    overlayDiv.classList.remove("hidden");
+}
 
-    // FUNCTION TO HOVER AND UNHOVER THE MINDHUNTER LOGO
-    function hoverImage(element, elementType){
-        if(elementType === 1){
-            element.setAttribute('src', '../src/assets/images/mindhunter_logo_white.png');
-        }else if(elementType === 2){
-            element.setAttribute('src', '../assets/images/mindhunter_logo_white.png');
-        }
-    }
-
-    function unhoverImage (element, elementType){
-        if(elementType === 1){
-            element.setAttribute('src', './src/assets/images/mindhunter_logo.svg');
-        }else if(elementType === 2){
-            element.setAttribute('src', './src/assets/images/mindhunter_logo.svg');
-        }
-    }
-
-    // FUNCTION TO HIDE THE MINDHUNTER POSTER AND SHOW THE TRAILER
-    function changeToVid(element) {
-        element.classList.add("hidden");
-        imagePoster.classList.add("hidden");
-        trailerVideo.classList.remove("hidden");
-        figCaption.innerHTML = `Tráiler oficial de Mindhunter. | Video proporcionado por <a href="https://www.youtube.com/watch?v=LR3G1lWbnUU" target="_blank">Netflix</a>`
-        trailerVideo.autoplay = "true";
-        trailerVideo.load();
-    }
-
-    //FUNCTION TO ADD AN OVERLAY WHEN HOVERING THE IMAGE
-    function addOverlay(element, e){
-        e.preventDefault();
-        overlayDiv.classList.remove("hidden");
-    }
-
-    function removeOverlay(element) {
-        overlayDiv.classList.add("hidden");
-    }
-});
-
-
+export function removeOverlay(element) {
+    overlayDiv.classList.add("hidden");
+}
 
 trailerVideo.addEventListener('ended', function() {
     // show image here
